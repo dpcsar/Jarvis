7a1b4b10f805b097e9175bfa9c99aadb6627b12b
dpcsar <dpxar@hotmail.com>
2025-05-23 16:28:55 -0700

    feat: Set minSdk to 33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "site.jarviscopilot.jarvis"
-        minSdk = 34
+        minSdk = 33
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
